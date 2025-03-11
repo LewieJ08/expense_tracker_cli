aa084c332a054e47c7377b5e10aa79e50200c3a8
LewieJ08 <lewiej08@gmail.com>
2025-03-11 09:22:10 +0000

    update setup.py and main.py

---
+++ expense_tracker_cli/main.py
@@ -0,0 +1,24 @@
+import os
+import json
+
+def add():
+    pass
+    
+def update():
+    pass
+
+def delete():
+    pass
+
+def view_all():
+    pass
+
+def view_summary():
+    pass
+
+def main():
+    os.system("cls")
+    command = input("Expense-Tracker> ").lower().strip()
+
+if __name__ == "__main__":
+    main()
