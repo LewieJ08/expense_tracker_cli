d9cd3a28de88434d135759ff36c34525fb74f176
LewieJ08 <lewiej08@gmail.com>
2025-03-13 10:34:26 +0000

    added 'add' function

--- expense_tracker_cli/main.py
+++ expense_tracker_cli/main.py
@@ -1,8 +1,35 @@
 import os
 import json
+import datetime
 
-def add():
-    pass
+JSONFILE = "data.json"
+COMMANDS = ["exit","add","update","delete","list","summary"]
+
+def current_date():
+    return str(datetime.datetime.now().strftime("%d/%m/%Y"))
+
+def add(description,amount):
+    with open(JSONFILE) as file:
+        data = json.load(file)
+    iD = 1
+    current_ids = []
+
+    for item in data:
+        current_ids.append(item["id"])
+
+    while iD in current_ids:
+        iD += 1
+
+    data.append({
+        "id": iD,
+        "date": current_date(),
+        "description": description,
+        "amount": amount
+    })
+
+    with open(JSONFILE,"w") as file:
+        json.dump(data,file,indent=4)
+    
     
 def update():
     pass
@@ -10,15 +37,34 @@ def update():
 def delete():
     pass
 
-def view_all():
+def summary():
     pass
 
-def view_summary():
+def list():
     pass
 
+#main function
 def main():
-    os.system("cls")
-    command = input("Expense-Tracker> ").lower().strip()
+    if not os.path.exists(JSONFILE):
+        with open(JSONFILE,"w") as file:
+            json.dump([], file)
+    while True:
+        uinput = input("Expense-Tracker> ").lower().strip()
 
+        splitInput = uinput.split(" ",2)
+
+        if splitInput[0] == COMMANDS[0]:
+            break
+
+        command, argument, amount = splitInput 
+      
+        if command == COMMANDS[1]:
+            try:
+                amount = int(amount)
+                add(argument, amount)
+            except ValueError:
+                print("Amount must be an integer ")
+
+#entry point
 if __name__ == "__main__":
     main()
